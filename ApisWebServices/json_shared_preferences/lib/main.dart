62993caa4e38f00cbb9980a3048fb68ef91dab3d
MarcoSilva2007 <marcoladesenvolvedor@gmail.com>
2025-08-12 11:22:27 -0300

    12.08.2025

--- ApisWebServices/json_shared_preferences/lib/main.dart
+++ ApisWebServices/json_shared_preferences/lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:json_shared_preferences/tela_inicial.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 
 void main() {
   runApp(MaterialApp(home: ConfigPage()));
@@ -16,13 +17,37 @@ class ConfigPage extends StatefulWidget {
 class _ConfigPageState extends State<ConfigPage> {
   // atributos
   bool temaEscuro = false;
+  String nomeUsuario = "";
 
+  @override
   void initState() {
     super.initState();
     carregarPreferencias();
   }
 
   void carregarPreferencias() async {
-    final prefs = await SharedPreferences.getInstance();
+    final prefs =
+        await SharedPreferences.getInstance(); // Conexão com o shared prefs
+    String? jsonString = prefs.getString(
+      "config",
+    ); // Recebendo os valores referentes a chave "config" do SP
+    if (jsonString != null) {
+      // Veifica se o jsonString esta vazio
+      Map<String, dynamic> config = json.decode(jsonString);
+      setState(() {
+        //  Método para a mudança de estado
+        temaEscuro = config["temaEscuro"] ?? false;
+        nomeUsuario = config["nome"] ?? "";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "App de Configuração",
+      theme: temaEscuro ? ThemeData.dark() : ThemeData.light(),
+      home: TelaInicial(temaEscuro: temaEscuro, nomeUsuario: nomeUsuario,),
+    );
   }
 }
